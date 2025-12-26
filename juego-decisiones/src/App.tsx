65d3b88b4c4ecfc656b93ec5ee3cf635b302058b
DESKTOP-HPS969V\Ana <diegogpr3@gmail.com>
2025-12-26 12:14:49 +0100

    Vertical warning mobile

--- juego-decisiones/src/App.tsx
+++ juego-decisiones/src/App.tsx
@@ -5,7 +5,11 @@ function App() {
   console.log('Renderizando App');
   return (
     <div className="app-container">
-      <main>
+      <div className="rotate-warning">
+        <div className="rotate-icon">⟳</div>
+        <p>Por favor, gira tu dispositivo en horizontal para jugar</p>
+      </div>
+      <main className="game-content">
         <Game />
       </main>
     </div>
